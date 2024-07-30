d5b6557cf69f2c2492cf529165d9e822e964e8ca
Avram Traian <traian.avram1@gmail.com>
2024-07-30 11:24:16 +0300

    Meta: Configure the build directories to be the same on all platforms
    Place both the build system files and binaries in the same directory,
    as well as removing the name of the used compiler from the output
    directory name.

--- Setup.bat
+++ Setup.bat
@@ -1,7 +1,14 @@
+::
 :: Copyright (c) 2024 Traian Avram. All rights reserved.
 :: SPDX-License-Identifier: BSD-3-Clause.
+::
 
 @echo off
+
+:: Variable that configure that project file generation.
+set compiler=cl
+set generator=Ninja
+
 :: Push the directory where the batch file is located, as it also represents
 :: the root of the project. This is done in order to ensure the same behaviour,
 :: no matter from where the script file is invoked.
@@ -13,11 +20,11 @@ if not exist "Build/" ( mkdir "Build" )
 pushd "Build"
 
 :: Create the configuration-specific directories, if they don't exist.
-if not exist "Debug-MSVC/" ( mkdir "Debug-MSVC" )
-if not exist "Release-MSVC/" ( mkdir "Release-MSVC" )
+if not exist "Debug/" ( mkdir "Debug" )
+if not exist "Release/" ( mkdir "Release" )
 
-cmake -S ../ -B "Debug-MSVC"   -DCMAKE_CXX_COMPILER=cl -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
-cmake -S ../ -B "Release-MSVC" -DCMAKE_CXX_COMPILER=cl -G "Ninja" -DCMAKE_BUILD_TYPE=Release
+cmake -S ../ -B "Debug"   -DCMAKE_CXX_COMPILER=%compiler% -G %generator% -DCMAKE_BUILD_TYPE=Debug
+cmake -S ../ -B "Release" -DCMAKE_CXX_COMPILER=%compiler% -G %generator% -DCMAKE_BUILD_TYPE=Release
 
 popd
 
